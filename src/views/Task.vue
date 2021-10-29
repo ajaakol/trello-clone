825e024240e6952e6582b6e4db076af12adc31ac
ajaakol <aleksanteri.jaakola@towermedia.fi>
2021-10-29 13:26:26 +0300

    Added functionality to add and edit tasks

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="task-view">
     <div class="flex flex-col flex-grow items-start justify-between px-4">
-      {{ task.name }}
+      <input
+        type="text"
+        class="p-2 mr-2 w-full flex-grow text-xl font-bold"
+        :value="task.name"
+        @change="updateTaskProperty($event, 'name')"
+        @keyup.enter="updateTaskProperty($event, 'name')"
+      />
+
+      <textarea
+        class="
+          relative
+          w-full
+          bg-transparent
+          px-2
+          border
+          mt-2
+          h-64
+          border-none
+          leading-normal
+        "
+        :value="task.description"
+        @change="updateTaskProperty($event, 'description')"
+      />
     </div>
   </div>
 </template>
@@ -14,6 +36,15 @@ export default {
     ...mapGetters(["getTask"]),
     task() {
       return this.getTask(this.$route.params.id);
+    },
+  },
+  methods: {
+    updateTaskProperty(e, key) {
+      this.$store.commit("UPDATE_TASK", {
+        task: this.task,
+        key,
+        value: e.target.value,
+      });
     },
   },
 };
