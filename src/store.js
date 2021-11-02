35030333b47aab78cf8a5dd731f4a443326bc8cd
ajaakol <aleksanteri.jaakola@towermedia.fi>
2021-11-02 09:18:23 +0200

    Fixed prettier + eslint

--- src/store.js
+++ src/store.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import defaultBoard from "./default-board";
-import { saveStatePlugin, uuid } from "./utils";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import defaultBoard from './default-board'
+import { saveStatePlugin, uuid } from './utils'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-const board = JSON.parse(localStorage.getItem("board")) || defaultBoard;
+const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
 
 export default new Vuex.Store({
   plugins: [saveStatePlugin],
   state: {
-    board,
+    board
   },
   getters: {
     getTask(state) {
@@ -18,33 +18,36 @@ export default new Vuex.Store({
         for (const column of state.board.columns) {
           for (const task of column.tasks) {
             if (task.id === id) {
-              return task;
+              return task
             }
           }
         }
-      };
-    },
+      }
+    }
   },
   mutations: {
     CREATE_TASK(state, { tasks, name }) {
       tasks.push({
         name,
         id: uuid(),
-        description: "",
-      });
+        description: ''
+      })
     },
+
     UPDATE_TASK(state, { task, key, value }) {
-      task[key] = value;
+      task[key] = value
       // Vue.set(task, key, value)
     },
+
     MOVE_TASK(state, { fromTasks, toTasks, fromTaskIndex, toTaskIndex }) {
-      const taskToMove = fromTasks.splice(fromTaskIndex, 1)[0];
-      toTasks.splice(toTaskIndex, 0, taskToMove);
+      const taskToMove = fromTasks.splice(fromTaskIndex, 1)[0]
+      toTasks.splice(toTaskIndex, 0, taskToMove)
     },
+
     MOVE_COLUMN(state, { fromColumnIndex, toColumnIndex }) {
-      const columnList = state.board.columns;
-      const columnToMove = state.board.columns.splice(fromColumnIndex, 1)[0];
-      columnList.splice(toColumnIndex, 0, columnToMove);
-    },
-  },
-});
+      const columnList = state.board.columns
+      const columnToMove = state.board.columns.splice(fromColumnIndex, 1)[0]
+      columnList.splice(toColumnIndex, 0, columnToMove)
+    }
+  }
+})
